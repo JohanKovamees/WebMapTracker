c147017a903662e4da0a11397469d6ac30f5e3f6
Johan Kövamees <johan.kovamees@gmail.com>
2023-04-24 20:28:54 +0200

    More bug fixes and added support for user specific map

--- be/src/flaskp.py
+++ be/src/flaskp.py
@@ -1,109 +1,133 @@
 import os
 from crud import get_all_countries, get_country, get_user, add_country_to_user, remove_country_from_user
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from sqlalchemy import MetaData, create_engine
+from contextlib import contextmanager
 from sqlalchemy.orm import sessionmaker
 from login import login, register
+
+@contextmanager
+def create_session():
+    session = sessionmaker(bind=engine)()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 meta = MetaData()
 dbase_path = 'sqlite:///' + os.getcwd() + '/tables.db'
 engine = create_engine(dbase_path)
 
-session = sessionmaker()
-session.configure(bind=engine)
 
-with session() as s:
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
 
-    def create_app(test_config=None):
-        app = Flask(__name__, instance_relative_config=True)
+    @app.route('/')
+    def startpage():
+        return 'StartPage'
 
-        @app.route('/')
-        def startpage():
-            return 'StartPage'
+    @app.route('/api/login', methods=['POST'])
+    def login_web():
 
-        @app.route('/api/login', methods=['POST'])
-        def login_web():
+        user_json = request.json
+        username = user_json["username"]
+        password = user_json["password"]
 
-            user_json = request.json
-            username = user_json["username"]
-            password = user_json["password"]
-
-            login_successful = login(username, password, s)
-            if login_successful:
-                return {
-                    "Response" : "Login successful"
-                }
-            else:
-                return {
-                    "Response" : "Login unsuccessful"
-                }
-
-        @app.route('/api/register', methods=['POST'])
-        def register_web():
-            user_json = request.json
-            username = user_json["username"]
-            password = user_json["password"]
-
-            register_successful = register(username, password, s)
-            if register_successful:
-                return {
-                    "Response" : "Registration successful"
-                }
-            else:
-                return {
-                    "Response" : "Registration unsuccessful"
-                }
-
-        @app.route('/api/countries', methods=['GET'])
-        def get_countries():
-            all_countries_string = get_all_countries(s)
-            return all_countries_string
+        with create_session() as session:
+            login_successful = login(username, password, session)
         
-        @app.route('/api/country/<country>', methods=['GET'])
-        def get_single_country(country):
-            country = get_country(country, s)
-            return country.name
-        
-        @app.route('/api/user/<user>', methods=['GET'])
-        def get_a_user(user):
-            user_string = get_user(user,s)
-            return user_string.name
+        if login_successful:
+            return {
+                "Response" : "Login successful"
+            }
+        else:
+            return {
+                "Response" : "Login unsuccessful"
+            }
 
-        @app.route('/api/user/<user>/countries/<abb>', methods=['POST'])
-        def add_country(user, abb):
-            dict = request.json
-            abb = dict["Country"]
-            user = dict["User"]
-            successful_add = add_country_to_user(user, abb, s)
-            if successful_add:
-                return {
-                    "Response" : f"{abb} added to {user}"
-                }
-            else:
-                return {
-                    "Response" : "Failed to add"
-                }
+    @app.route('/api/register', methods=['POST'])
+    def register_web():
+        user_json = request.json
+        username = user_json["username"]
+        password = user_json["password"]
 
-        '''@app.route('/user/<user>/countries', methods=['POST'])
-        def add_country2(user):
-            abb = "US"
-            print(request.json)
-            add_country_to_user(user, abb, s)
-            return "Added " + abb + " to " + user'''
+        with create_session() as session:
+            register_successful = register(username, password, session)
+        if register_successful:
+            return {
+                "Response" : "Registration successful"
+            }
+        else:
+            return {
+                "Response" : "Registration unsuccessful"
+            }
 
-        @app.route('/api/user/<user>/countries/<abb>', methods=['DELETE'])
-        def delete_country(user, abb):
-            dict = request.json
-            abb = dict["Country"]
-            user = dict["User"]
-            successful_remove = remove_country_from_user(user, abb, s)
-            if successful_remove:
-                return {
-                    "Response" : f"{abb} removed from {user}"
-                }
-            else:
-                return {
-                    "Response" : "Failed to remove"
-                }
-        
-        return app
+    @app.route('/api/countries', methods=['GET'])
+    def get_countries():
+        with create_session() as session:
+            all_countries_string = get_all_countries(session)
+        return all_countries_string
+    
+    @app.route('/api/country/<country>', methods=['GET'])
+    def get_single_country(country):
+        with create_session() as session:
+            country = get_country(country, session)
+        return country.name
+    
+    @app.route('/api/user/<user>', methods=['GET'])
+    def get_a_user(user):
+        user_string = get_user(user,s)
+        return user_string.name
+
+    @app.route('/api/user/<user>/countries/<abb>', methods=['POST'])
+    def add_country(user, abb):
+        dict = request.json
+        abb = dict["Country"]
+        user = dict["User"]
+        with create_session() as session:
+            successful_add = add_country_to_user(user, abb, session)
+        if successful_add:
+            return {
+                "Response" : f"{abb} added to {user}"
+            }
+        else:
+            return {
+                "Response" : "Failed to add"
+            }
+
+
+    @app.route('/api/map-data', methods=['GET'])
+    def map_data():
+        username = request.args.get('username')
+        if not username:
+            return jsonify({"Error": "Missing username parameter"}), 400
+
+        with create_session() as session:
+            user = get_user(username, session)
+        if not user:
+            return jsonify({"Error": "User not found"}), 404
+
+        visited_countries = [country.abb for country in user.countries]
+        return jsonify({"visitedCountries": visited_countries})
+
+    @app.route('/api/user/<user>/countries/<abb>', methods=['DELETE'])
+    def delete_country(user, abb):
+        dict = request.json
+        abb = dict["Country"]
+        user = dict["User"]
+        with create_session() as session:
+            successful_remove = remove_country_from_user(user, abb, session)
+        if successful_remove:
+            return {
+                "Response" : f"{abb} removed from {user}"
+            }
+        else:
+            return {
+                "Response" : "Failed to remove"
+            }
+    
+    return app
