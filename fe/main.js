f57a2fb24c4ae64de731739798728fe807ec7589
Johan Kövamees <johan.kovamees@gmail.com>
2023-04-24 14:27:28 +0200

    Included FE for app. Changed structure to include FE in the same repo. Still different docker images

---
+++ fe/main.js
@@ -0,0 +1,31 @@
+// Initialize map and set view
+const map = L.map('map').setView([51.505, -0.09], 2);
+
+// Add a base layer to the map (use any other tile provider you prefer)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// Function to handle clicks on countries
+function onCountryClick(e) {
+  const countryAbb = e.target.feature.properties.iso_a2;
+  const username = 'your_username'; // Replace with the actual username
+
+  // Add or remove a country
+  if (e.originalEvent.ctrlKey) {
+    axios.delete(`/user/${username}/countries/${countryAbb}`);
+  } else {
+    axios.post(`/user/${username}/countries/${countryAbb}`);
+  }
+}
+
+// Load GeoJSON data for countries and add to map
+axios.get('https://unpkg.com/world-atlas@2.0.0-preview.4/countries-50m.json')
+  .then(response => {
+    const countriesLayer = L.geoJSON(response.data, {
+      onEachFeature: (feature, layer) => {
+        layer.on('click', onCountryClick);
+      },
+    });
+    countriesLayer.addTo(map);
+  });
